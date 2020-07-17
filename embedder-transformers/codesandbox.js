d9636bcf205bc144aeaf8c6542796d33bf486c79
Matias Hernandez <matiasfh@gmail.com>
2020-07-16 23:58:33 -0400

    Embedders

---
+++ embedder-transformers/codesandbox.js
@@ -0,0 +1,37 @@
+const { URL } = require("url");
+
+const shouldTransform = (url) => {
+  const { host, pathname } = new URL(url);
+
+  return host === "codesandbox.io" && !pathname.includes("/embed");
+};
+
+const getCodeSandboxIFrameSrc = (url) => {
+  const { host, pathname, searchParams } = new URL(url);
+
+  // Don't preload videos
+  if (!searchParams.has("preload")) {
+    searchParams.set("preload", false);
+  }
+
+  return `https://${host}${pathname}/embed?${searchParams.toString()}`;
+};
+
+const getHTML = (url) => {
+  const iframeSrc = getCodeSandboxIFrameSrc(url);
+
+  return `
+    <div style="position: relative;">
+      <div style="height: 0px; padding-bottom: 56.25%;">
+        <iframe
+          src="${iframeSrc}"
+          style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;position:absolute;top:0 left:0; border:none; border-radius: 4px"
+          allowfullscreen
+        >
+        </iframe>
+      </div>
+    </di>
+  `;
+};
+
+module.exports = { getHTML, shouldTransform };
---
+++ embedder-transformers/egghead.js
@@ -0,0 +1,41 @@
+const { URL } = require("url");
+
+const shouldTransform = (url) => {
+  const { host, pathname } = new URL(url);
+
+  return (
+    host === "egghead.io" &&
+    pathname.includes("/lessons/") &&
+    !pathname.includes("/embed")
+  );
+};
+
+const getEggheadIFrameSrc = (url) => {
+  const { host, pathname, searchParams } = new URL(url);
+
+  // Don't preload videos
+  if (!searchParams.has("preload")) {
+    searchParams.set("preload", false);
+  }
+
+  return `https://${host}${pathname}/embed?${searchParams.toString()}`;
+};
+
+const getHTML = (url) => {
+  const iframeSrc = getEggheadIFrameSrc(url);
+
+  return `
+    <div style="position: relative;">
+      <div style="height: 0px; padding-bottom: 56.25%;">
+        <iframe
+          src="${iframeSrc}"
+          style="position: absolute; width: 100%; height: 100%; top: 0px; left: 0px; border: none; border-radius: 4px;"
+          allowfullscreen
+        >
+        </iframe>
+      </div>
+    </di>
+  `;
+};
+
+module.exports = { getHTML, shouldTransform };
