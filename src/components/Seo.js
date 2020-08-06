6d7ad5dfa5e1fe738dacf9a9a546b0d12bf8fd24
Matias Hernandez <matiasfh@gmail.com>
2020-08-06 16:28:13 -0400

    fix SEO

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -30,10 +30,6 @@ const Seo = ({ title, frontmatter }) => {
   const postImage = frontmatter?.banner?.childImageSharp.fluid.src;
   const postDescription = frontmatter?.description;
 
-  let metaTitle = siteTitle;
-  if (postTitle) {
-    metaTitle = postTitle;
-  }
   let metaDescription = description;
   if (postDescription) {
     metaDescription = `${postDescription.substring(0, 157)}...`;
@@ -43,22 +39,24 @@ const Seo = ({ title, frontmatter }) => {
   if (postImage) {
     metaImage = postImage;
   }
+  let realTitle = siteTitle;
+  if (title) {
+    realTitle = `${title} | ${siteTitle}`;
+  }
   return (
     <Helmet
-      title={metaTitle}
+      title={realTitle}
       meta={[
         { name: "description", content: metaDescription },
         { name: "keywords", content: frontmatter.keywords || keywords },
       ]}
     >
       <html lang="en" />
-      <title>
-        {siteTitle} | {metaTitle}
-      </title>
+      <title>{realTitle}</title>
       <meta name="description" content={metaDescription} />
       {/* OpenGraph tags */}
       <meta property="og:url" content={url} />
-      <meta property="og:title" content={`${siteTitle} | ${metaTitle}`} />
+      <meta property="og:title" content={realTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:image" content={metaImage} />
       {/*<meta property="fb:app_id" content={seo.social.fbAppID} />*/}
@@ -66,7 +64,7 @@ const Seo = ({ title, frontmatter }) => {
       {/* Twitter Card tags */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitter} />
-      <meta name="twitter:title" content={`${siteTitle} | ${metaTitle}`} />
+      <meta name="twitter:title" content={realTitle} />
       <meta name="twitter:description" content={metaDescription} />
       <meta name="twitter:image" content={metaImage} />
       <noscript>This site runs best with JavaScript enabled.</noscript>
